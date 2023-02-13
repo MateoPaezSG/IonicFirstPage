e2ca0461163e2feed97cf6d7f2b7cfc287681306
Chapa Daniel <Chapadaniel479@gmail.com>
2023-02-13 11:39:36 -0500

    Ionico

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['style.css']
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Alerta de Ionic',
+      message: 'Hola desde Alert Ionic',
+      buttons: ['OK', 'Cancel'],
+    });
+  
+    await alert.present();
+  }
+  async presentAlerto() {
+    const alert = await this.alertController.create({
+      header: 'hola',
+      subHeader: 'ELbicho123',
+      message: 'Viva Messi',
+      buttons: ['ua', 'au'],
+    });
+  
+    await alert.present();
+  }
+  async presentAlertodo() {
+    const alert = await this.alertController.create({
+      header: 'CUIDAO',
+      subHeader: 'Mensaje',
+      message: 'ay me tocaste',
+      buttons: ['hola', 'mundo'],
+    });
+  
+    await alert.present();
+  }
 }
